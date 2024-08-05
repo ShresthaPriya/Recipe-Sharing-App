34dde15c75359529a482d2b49d192ea5c274929e
Priya Shrestha <NP03CS4A220148@heraldcollege.edu.np>
2024-08-05 09:49:02 +0545

    using get method and testing the api in postman

---
+++ Backend/routes/recipe.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+router.get("/", (req, res) => {
+    res.json({ success: true, message: "Hello" });
+  });
+
+module.exports=router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
-const express = require("express");
+const express = require("express"); // Importing express
+const app = express(); // Calling express 
+const dotenv = require("dotenv").config();
+const recipe = require("./routes/recipe");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Middleware for parsing JSON
+
+app.use("/", recipe); // Use the recipe router
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Hello" });
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error starting server:', err);
+  } else {
+    console.log(`Starting server on port ${PORT}`);
+  }
+});
